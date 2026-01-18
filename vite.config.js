a7e2d309a32dac65b045a1fd218da619ac085edd
Da1zy <romapetrusenko9@gmail.com>
2026-01-18 23:53:02 +0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,8 @@
-import { resolve } from 'path'
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vite';
 
 export default defineConfig({
-	root: '.',
-	build: {
-		outDir: 'dist',
-		emptyOutDir: true,
-		rollupOptions: {
-			input: resolve(__dirname, 'index.html'),
-		},
-	},
-	server: {
-		open: true,
-		port: 3000,
-	},
-})
+  server: {
+    open: true,
+  },
+  base: './',
+});
